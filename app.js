b59e222489e7ff9432ec109f7177f0bf2c8b5afc
Timothy Yuan <timothyyuan@Timothys-MBP.hsd1.ca.comcast.net>
2018-09-01 15:16:49 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var user = require('./routes/user.js');
+app.use(bodyParser.json() );       
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/user', user);
+
+app.get('/image/:image', function(req, res) {
+  res.sendFile(path.join(__dirname, '/uploads/'+req.params.image));
+});
+
+
+app.listen(3000);
